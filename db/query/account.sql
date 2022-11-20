eff26a03c3204ff9cbf05e150663fa6e2a01c1ba
dai0115 <hateen0808@gmail.com>
2022-11-20 13:56:04 +0900

    updateAccount method returns Accont object

--- db/query/account.sql
+++ db/query/account.sql
@@ -18,10 +18,11 @@ ORDER BY id
 LIMIT $1
 OFFSET $2;
 
--- name: UpdateAccount :exec
+-- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $2
-where id = $1;
+where id = $1
+RETURNING *;
 
 -- name: DeleteAccount :exec
 DELETE FROM accounts WHERE id = $1;
